e867abb561deb3c49e3d24d27330b9609dfa833b
HanZamai <lizkinmir93@gmail.com>
2025-06-10 00:56:42 +0400

    civ-40-jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,49 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Request, Post, UseGuards, Body, UnauthorizedException, Get } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
-  @Post('/login')
+  constructor(
+    private authService: AuthService,
+    private jwtService: JwtService,
+  ) {}
+  
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
+  
+  @UseGuards(LocalAuthGuard)
+  @Post('logout')
+  async logout(@Request() req) {
+    return req.logout();
+  }
+
+  @Post('refresh')
+  async refresh(@Body() { refreshToken }: { refreshToken: string }) {
+    try {
+      const payload = this.jwtService.verify(refreshToken, {
+        secret: process.env.JWT_REFRESH_SECRET,
+      });
+
+      const newAccessToken = this.jwtService.sign(
+        { username: payload.username, sub: payload.sub },
+        { secret: process.env.JWT_ACCESS_SECRET, expiresIn: '15m' }
+      );
+
+      return { access_token: newAccessToken };
+    } catch (e) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    const accessToken = this.jwtService.sign(payload, {
+        secret: process.env.JWT_ACCESS_SECRET,
+        expiresIn: '15m',
+      });
+    
+      const refreshToken = this.jwtService.sign(payload, {
+        secret: process.env.JWT_REFRESH_SECRET,
+        expiresIn: '7d',
+      });
+    
+      return {
+        access_token: accessToken,
+        refresh_token: refreshToken,
+      };
+  }
+}
